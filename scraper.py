9d44349d8a1d793378b2adbdc0adbd60d63b13b9
Divyansh Tripathi <85444838+divyansh-tripathi7@users.noreply.github.com>
2023-08-19 16:39:25 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,11 +10,9 @@ for index, page_url in enumerate(page_urls):
     full_url = f"{base_url}/{page_url}"
     response = requests.get(full_url)
     soup = BeautifulSoup(response.content, "html.parser")
-    page_text = soup.prettify()  # Use prettify to maintain HTML formatting
+    page_text = soup.prettify()  
 
     pdf_filename = f"page_{index+1}.pdf"
-
-    # Create a PDF using WeasyPrint
     HTML(string=page_text).write_pdf(pdf_filename)
 
     print(f"PDF created for {full_url} as {pdf_filename}")
